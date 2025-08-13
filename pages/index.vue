1a5274ac099b706190abcd1d82341ae05742d2e2
hossein vkh <hosseinvkhdev@gmail.com>
2025-08-13 12:19:37 +0330

    wip: create store

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,13 @@
-<script setup></script>
+<script setup>
+const { data, status, error } = await useFetch(
+  "https://jsonplaceholder.typicode.com/users"
+);
+
+console.log(data.value, status.value, error.value);
+</script>
+
 <template>
-  <h1 class="text-3xl text-sec font-bold w-fit bg-red-500">lorem10</h1>
+  <div class="p-4">
+    <h1 class="text-3xl text-sec font-bold w-fit mb-4">Users List</h1>
+  </div>
 </template>
